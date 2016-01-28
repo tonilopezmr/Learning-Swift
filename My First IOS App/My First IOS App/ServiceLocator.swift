328fd8b71c1989cf083e4d42cde756ca27a37315
Antonio López Marín <tonilopezmr@gmail.com>
2016-01-28 18:10:18 +0100

    created UI and ServiceLocator

---
+++ My First IOS App/My First IOS App/ServiceLocator.swift
@@ -0,0 +1,54 @@
+//
+//  ServiceLocator.swift
+//  My First IOS App
+//
+//  Created by Antonio López Marín on 28/01/16.
+//  Copyright © 2016 Antonio López Marín. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ServiceLocator {
+    
+    private func provideDataSource() -> MockDataSource{
+        return MockDataSource()
+    }
+    
+    private func provideSubjectRepository() -> SubjectRespositoryProtocol {
+        return SubjectRepository(datasource: provideDataSource())
+    }
+
+    private func provideUseCases() -> UseCase {
+        let repository = provideSubjectRepository()
+        let getSubjectList = GetSubjects(repository: repository)
+        let getSubject = GetSubject(repository: repository)
+        let createSubject = CreateSubject(repository: repository)
+        let deleteSubject = DeleteSubject(repository: repository)
+        let updateSubject = UpdateSubject(repository: repository)
+        return UseCase(getSubjectList: getSubjectList, getSubject: getSubject, createSubject: createSubject, deleteSubject: deleteSubject, updateSubject: updateSubject)
+    }
+    
+    private func provideSubjectPresenter(ui: SubjectUI) -> SubjectPresenter {
+        return SubjectPresenter(ui: ui, useCase: provideUseCases())
+    }
+    
+    private func provideStoryBoard() -> UIStoryboard{
+        return UIStoryboard(name: "Main", bundle: NSBundle.mainBundle())
+    }
+    
+    func provideRootViewController() -> UIViewController{
+        let uiStoryBoard = provideStoryBoard()
+        let navigationController = uiStoryBoard.instantiateInitialViewController() as! UINavigationController
+        navigationController.viewControllers = [provideSubjectController()]
+        return navigationController
+    }
+    
+    func provideSubjectController() -> UIViewController {
+        let uiStoryBoard = provideStoryBoard()
+        let subjectViewController: ViewController = uiStoryBoard.instantiateViewControllerWithIdentifier("ViewController") as! ViewController
+        subjectViewController.presenter = provideSubjectPresenter(subjectViewController)
+        return subjectViewController
+    }
+    
+}
--- My First IOS App/My First IOS App/ViewController.swift
+++ My First IOS App/My First IOS App/ViewController.swift
@@ -8,23 +8,77 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, SubjectUI {
+    
+    var items: [Subject] = []
     
     @IBOutlet var subjectName: UITextField!
     @IBOutlet var emptyCaseView: UILabel!
     @IBOutlet var tableView: UITableView!
     
+    var presenter: SubjectPresenter! = nil
+    
     override func viewDidLoad() {
+        emptyCaseView.hidden = true
+        tableView.accessibilityLabel = "SubjectTableView"
+        tableView.accessibilityIdentifier = "SubjectTableView"
+        presenter.viewDidLoad()
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
     }
 
     @IBAction func onAddButtonClick(sender: UIButton) {
+        let subjectName = self.subjectName.text ?? "No subject"
+        let subject = Subject(id: items.count, name: subjectName)
+        presenter.createSubject(subject)
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func showEmptyCase(){
+        emptyCaseView.hidden = false
+    }
+    
+    func showItems(items: [Subject]) {
+        emptyCase(items)
+        self.items = items
+        tableView.reloadData()
+    }
+    
+    func showNewItem(item: Subject){
+        emptyCase(items)
+        self.items.append(item)
+        tableView.reloadData()
+    }
+    
+    func showLoader() {}
+    func hideLoader() {}
+    
+    func showMessage(message: String){}
+    
+    private func emptyCase(items: [Subject]){
+        self.emptyCaseView.hidden = items.count > 0
+    }
+    
+    //TableView
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.items.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell:UITableViewCell = self.tableView.dequeueReusableCellWithIdentifier("cell")! as UITableViewCell
+        
+        cell.textLabel?.text = self.items[indexPath.row].name
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        print("You selected cell #\(indexPath.row)!")
     }
 }
 
